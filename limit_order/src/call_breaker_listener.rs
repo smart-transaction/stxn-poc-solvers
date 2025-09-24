8a829f6f4556825d1d59806de281e526ce1df8b3
TokenTitan <r@stxn.io>
2025-09-24 15:34:17 +0530

    feat: added callBreaker listener

--- limit_order/src/call_breaker_listener.rs
+++ limit_order/src/call_breaker_listener.rs
@@ -11,21 +11,21 @@ use tokio::{
 };
 
 use crate::{
-    contracts_abi::laminator::{Laminator, ProxyPushedFilter},
+    contracts_abi::call_breaker::{CallBreaker, UserObjectivePushedFilter},
     solver::{selector, SolverParams},
-    solvers::limit_order::{self, LimitOrderSolver},
+    solvers::limit_order::LimitOrderSolver,
     stats::TimerExecutorStats,
     timer_executor::TimerRequestExecutor,
 };
 
-pub struct LaminatorListener<M: Clone> {
-    // The address of the laminator contract.
-    laminator_address: Address,
+pub struct CallBreakerListener<M: Clone> {
+    // The address of the call breaker contract.
+    call_breaker_address: Address,
 
     // The middleware to be used
     middleware: Arc<M>,
 
-    // Mapping of app selectors to solver params.
+    // Mapping of app IDs to solver params.
     solvers_params: HashMap<H256, SolverParams<M>>,
 
     // JoinSet for using for executors spawning.
@@ -38,17 +38,17 @@ pub struct LaminatorListener<M: Clone> {
     stats_tx: Sender<TimerExecutorStats>,
 }
 
-impl<M: Middleware + Clone + 'static> LaminatorListener<M> {
+impl<M: Middleware + Clone + 'static> CallBreakerListener<M> {
     pub fn new(
-        laminator_address: Address,
+        call_breaker_address: Address,
         middleware: Arc<M>,
         solvers_params: HashMap<H256, SolverParams<M>>,
         exec_set: Arc<Mutex<JoinSet<()>>>,
         tick_duration: Duration,
         stats_tx: Sender<TimerExecutorStats>,
-    ) -> LaminatorListener<M> {
-        LaminatorListener::<M> {
-            laminator_address,
+    ) -> CallBreakerListener<M> {
+        CallBreakerListener::<M> {
+            call_breaker_address,
             middleware,
             solvers_params,
             exec_set,
@@ -58,30 +58,31 @@ impl<M: Middleware + Clone + 'static> LaminatorListener<M> {
     }
 
     pub async fn listen(&mut self) {
-        let laminator_contract = Laminator::new(self.laminator_address, self.middleware.clone());
-        let events = laminator_contract
-            .event::<ProxyPushedFilter>()
+        let call_breaker_contract =
+            CallBreaker::new(self.call_breaker_address, self.middleware.clone());
+        let events = call_breaker_contract
+            .event::<UserObjectivePushedFilter>()
             .from_block(BlockNumber::Latest);
         loop {
             match events.stream().await {
                 Ok(stream) => {
                     let mut stream_take = stream.take(10);
-                    println!("Listening the event ProxyPushed ...");
-                    while let Some(Ok(proxy_pushed)) = stream_take.next().await {
-                        if let Some(solver_params) =
-                            self.solvers_params.get(&proxy_pushed.selector.into())
-                        {
+                    println!("Listening the event UserObjectivePushed ...");
+                    while let Some(Ok(user_objective_pushed)) = stream_take.next().await {
+                        let app_id: H256 = user_objective_pushed.app_id;
+
+                        if let Some(solver_params) = self.solvers_params.get(&app_id) {
                             let mut exec_set = self.exec_set.lock().await;
                             let solver_params = solver_params.clone();
                             let tick_duration = self.tick_duration.clone();
                             let stats_tx = self.stats_tx.clone();
                             exec_set.spawn(async move {
-                                let limit_order_selector =
-                                    selector(limit_order::APP_SELECTOR.to_string());
-                                let event_selector: H256 = proxy_pushed.selector.into();
-                                if event_selector == limit_order_selector {
+                                let limit_order_app_id =
+                                    selector("FLASHLIQUIDITY.LIMITORDER".to_string());
+                                let event_app_id: H256 = user_objective_pushed.app_id;
+                                if event_app_id == limit_order_app_id {
                                     let limit_order_solver = LimitOrderSolver::new(
-                                        proxy_pushed.clone(),
+                                        user_objective_pushed.clone(),
                                         solver_params.clone(),
                                     );
                                     if let Ok(limit_order_solver) = limit_order_solver {
@@ -91,7 +92,7 @@ impl<M: Middleware + Clone + 'static> LaminatorListener<M> {
                                                 tick_duration,
                                                 stats_tx,
                                             );
-                                        executor.execute(proxy_pushed).await;
+                                        executor.execute(user_objective_pushed).await;
                                     } else {
                                         println!("Error creating solver: Unknown selector");
                                     }
